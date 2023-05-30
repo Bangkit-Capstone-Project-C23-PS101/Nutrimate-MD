7f56baddf05971367385a51b08e220bf92f8bc1a
azfarrisla <azfar.risla@yahoo.co.id>
2023-05-30 17:57:58 +0700

    Adding Home and Password Layout and Activity

--- app/src/main/java/com/example/nutrimate/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/nutrimate/ui/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.example.nutrimate
+package com.example.nutrimate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nutrimate.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
