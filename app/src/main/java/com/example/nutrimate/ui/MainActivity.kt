aa5fe8ab31207bd70a7035177d40db34a59aa591
azfarrisla <azfar.risla@yahoo.co.id>
2023-05-31 23:32:15 +0700

    Adding Camera Activity

--- app/src/main/java/com/example/nutrimate/ui/MainActivity.kt
+++ app/src/main/java/com/example/nutrimate/ui/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.nutrimate.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nutrimate.R
+import com.example.nutrimate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonAdd.setOnClickListener{
+            val intent = Intent(this@MainActivity,PhotoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
